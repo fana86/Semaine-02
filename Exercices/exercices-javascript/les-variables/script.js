644f5f3299f7af42d37b4abbabf033779c2c3e77
fana86 <67003052+fana86@users.noreply.github.com>
2020-06-17 12:09:22 +0200

    update

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,24 @@
+let helloWorld = 'hello world';
+console.log(helloWorld);
+let bonjour = "bonjour je m'appelle";
+console.log(bonjour);
+const firstName = 'Aliyou';
+
+console.log(bonjour + ' ' + firstName);
+let age = ' 34 ans ';
+console.log(age);
+let siriusStudentArray = [
+  'Aliyou',
+  'Lionel',
+  'kambou',
+  'Ezickel',
+  'Thierry',
+  'jean',
+  'Julie',
+  'Cissé',
+  'geremy',
+  'Alpha',
+];
+console.log(siriusStudentArray[6]);
+console.log(siriusStudentArray[0]);
+console.log(student.name);
